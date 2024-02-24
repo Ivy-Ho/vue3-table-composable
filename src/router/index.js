d32861f63b99884a41a2a496d199a8570a61300e
ivy.ho <ivy.ho@udngroup.com>
2024-02-24 16:49:26 +0800

    add path redirect

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      redirect: '/user_table'
+    },
+    {
       path: '/user_table',
       name: 'user_table',
       component: () => import('@/views/UserTableView.vue')
@@ -12,6 +16,10 @@ const router = createRouter({
       path: '/product_table',
       name: 'product_table',
       component: () => import('@/views/ProductTableView.vue')
+    },
+    {
+      path: '/:catchAll(.*)',
+      redirect: '/user_table'
     }
   ]
 })
